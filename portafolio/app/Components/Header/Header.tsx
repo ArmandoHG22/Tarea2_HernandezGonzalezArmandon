094115a9ea6d60a444b8bd110242bb07b37cc6de
ArmandoHG22 <90911277+ArmandoHG22@users.noreply.github.com>
2026-04-24 07:06:46 +0000

    Correccion de error cometido en el modulo css

--- portafolio/app/Components/Header/Header.tsx
+++ portafolio/app/Components/Header/Header.tsx
@@ -24,29 +24,29 @@ export default function Header(){
                 
                 <ul className={styles.nav__list}>
                     <li className={styles.nav__item}>
-                        <label htmlFor="nav-toggle">
+                        
                             <Link href="#home" className={styles.nav__link}>Inicio</Link>
-                        </label>
+                        
                     </li>
                     <li className={styles.nav__item}>
-                        <label htmlFor="nav-toggle">
+                        
                             <Link href="#interests" className={styles.nav__link}>Intereses</Link>
-                        </label>
+                        
                     </li>
                     <li className={styles.nav__item}>
-                        <label htmlFor="nav-toggle">
+                        
                             <Link href="#cv" className={styles.nav__link}>CV</Link>
-                        </label>
+                        
                     </li>
                     <li className={styles.nav__item}>
-                        <label htmlFor="nav-toggle">
+                        
                             <Link href="#projects" className={styles.nav__link}>Proyectos</Link>
-                        </label>
+                     
                     </li>
                     <li className={styles.nav__item}>
-                        <label htmlFor="nav-toggle">
+                        
                             <Link href="#contact" className={styles.nav__link}>Contacto</Link>
-                        </label>
+                       
                     </li>
                 </ul>
             </div>
